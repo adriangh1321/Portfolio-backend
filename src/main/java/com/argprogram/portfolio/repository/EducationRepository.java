fb7bf53afd76395e5a311d9785e9e55c73ee65e6
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-04 16:24:12 -0300

    created all repositories

---
+++ src/main/java/com/argprogram/portfolio/repository/EducationRepository.java
@@ -0,0 +1,11 @@
+
+package com.argprogram.portfolio.repository;
+
+import com.argprogram.portfolio.model.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EducationRepository extends JpaRepository<Education, Long>{
+    
+}
