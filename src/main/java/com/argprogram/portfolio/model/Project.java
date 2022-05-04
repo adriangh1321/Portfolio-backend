2c10b38a69c33d85d823d57f9b0c8023632416d6
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-04 12:55:51 -0300

    created Project model

---
+++ src/main/java/com/argprogram/portfolio/model/Project.java
@@ -0,0 +1,38 @@
+
+package com.argprogram.portfolio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "projects")
+public class Project {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="name")
+    private String name;
+    
+    @Column(name="description")
+    private String description;
+    
+    @ManyToOne
+    @JoinColumn(name="portfolio_id")
+    private Portfolio portfolio;
+}
