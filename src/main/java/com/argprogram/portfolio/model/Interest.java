74a4e2ada256b85cac28b59448cf4753a7c4e790
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-04 13:00:14 -0300

    created Interest model

---
+++ src/main/java/com/argprogram/portfolio/model/Interest.java
@@ -0,0 +1,38 @@
+
+package com.argprogram.portfolio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "interests")
+public class Interest {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="name")
+    private String name;
+    
+    @Column(name="image")
+    private String image;
+    
+    @ManyToOne
+    @JoinColumn(name="portfolio_id")
+    private Portfolio portfolio;
+}
