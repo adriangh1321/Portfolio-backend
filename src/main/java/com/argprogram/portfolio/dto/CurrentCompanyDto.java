e32ba85f0a29f6dc3f1a893924c3b5ce2b07f2e4
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 16:21:59 -0300

    created CurrentCompanyDto

---
+++ src/main/java/com/argprogram/portfolio/dto/CurrentCompanyDto.java
@@ -0,0 +1,14 @@
+
+package com.argprogram.portfolio.dto;
+
+
+public class CurrentCompanyDto {
+    
+    private Long id;
+    
+    private String name;
+    
+    private String image;
+    
+    private String url;
+}
