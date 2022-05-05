b4d1b704660ee494edc9dce171df053b7885821d
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 16:52:42 -0300

    created EducationMapper

---
+++ src/main/java/com/argprogram/portfolio/mapper/EducationMapper.java
@@ -0,0 +1,14 @@
+
+package com.argprogram.portfolio.mapper;
+
+import com.argprogram.portfolio.dto.EducationDto;
+import com.argprogram.portfolio.model.Education;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface EducationMapper {
+    
+    EducationDto toEducationDto(Education education);
+    
+    Education toEducation(EducationDto educationDto);
+}
