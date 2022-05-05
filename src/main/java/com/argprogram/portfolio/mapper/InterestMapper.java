315f84fa4505018fb3c578ec148c1198a4811c4b
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 17:14:37 -0300

    created InterestMapper

---
+++ src/main/java/com/argprogram/portfolio/mapper/InterestMapper.java
@@ -0,0 +1,14 @@
+
+package com.argprogram.portfolio.mapper;
+
+import com.argprogram.portfolio.dto.InterestDto;
+import com.argprogram.portfolio.model.Interest;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface InterestMapper {
+    
+    InterestDto toInterestDto(Interest interest);
+    
+    Interest toInterest(InterestDto interestDto);
+}
