0bf427119912cbf62c0e0787a7bf7704baa4f974
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 20:10:54 -0300

    created PortfolioDto

---
+++ src/main/java/com/argprogram/portfolio/dto/PortfolioDto.java
@@ -0,0 +1,51 @@
+package com.argprogram.portfolio.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PortfolioDto {
+
+    private Long id;
+    
+    @NotBlank
+    private String firstname;
+    
+    @NotBlank
+    private String lastname;
+    
+    @NotBlank
+    private String ocupation;
+
+    @NotBlank
+    private String country;
+
+    @NotBlank
+    private String state;
+
+    private String image;
+
+    @NotBlank
+    private String aboutMe;
+
+    private CurrentCompanyDto currentCompany;
+
+    private ContactInformationDto information;
+
+    private List<ExperienceDto> experiences = new ArrayList();
+
+    private List<EducationDto> educations = new ArrayList();
+
+    private List<SkillDto> skills = new ArrayList();
+
+    private List<ProjectDto> projects = new ArrayList();
+
+    private List<InterestDto> interests = new ArrayList();
+
+}
