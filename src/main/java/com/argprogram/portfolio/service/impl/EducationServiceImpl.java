532888dafded1faac7d28f9fbb0314e188aff118
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 16:59:54 -0300

    added getById in EducationService

--- src/main/java/com/argprogram/portfolio/service/impl/EducationServiceImpl.java
+++ src/main/java/com/argprogram/portfolio/service/impl/EducationServiceImpl.java
@@ -1,10 +1,26 @@
-
 package com.argprogram.portfolio.service.impl;
 
+import com.argprogram.portfolio.dto.EducationDto;
+import com.argprogram.portfolio.mapper.EducationMapper;
+import com.argprogram.portfolio.repository.EducationRepository;
 import com.argprogram.portfolio.service.EducationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EducationServiceImpl implements EducationService {
     
+    private final EducationRepository educationRepository;
+    private final EducationMapper educationMapper;
+
+    @Override
+    public EducationDto getById(Long id) {
+        EducationDto dto = this.educationRepository.findById(id)
+                .map(entity -> this.educationMapper.toEducationDto(entity))
+                .orElseThrow();
+        return dto;
+
+    }
+
 }
