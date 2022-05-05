9a7fcbfba7ce14c97cad9863a02908b274213c0a
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 17:42:57 -0300

    added getById in ProjectService

--- src/main/java/com/argprogram/portfolio/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/argprogram/portfolio/service/impl/ProjectServiceImpl.java
@@ -1,10 +1,25 @@
-
 package com.argprogram.portfolio.service.impl;
 
+import com.argprogram.portfolio.dto.ProjectDto;
+import com.argprogram.portfolio.mapper.ProjectMapper;
+import com.argprogram.portfolio.repository.ProjectRepository;
 import com.argprogram.portfolio.service.ProjectService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
-    
+
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    @Override
+    public ProjectDto getById(Long id) {
+        ProjectDto dto = this.projectRepository.findById(id)
+                .map(entity -> this.projectMapper.toProjectDto(entity))
+                .orElseThrow();
+        return dto;
+    }
+
 }
