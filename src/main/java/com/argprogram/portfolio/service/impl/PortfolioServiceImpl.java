4488e0edf5ffb2717f8024205d362506ff6b41f1
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-05 21:32:51 -0300

    added getAllByPortfolioId in EducationService

--- src/main/java/com/argprogram/portfolio/service/impl/PortfolioServiceImpl.java
+++ src/main/java/com/argprogram/portfolio/service/impl/PortfolioServiceImpl.java
@@ -1,9 +1,19 @@
 package com.argprogram.portfolio.service.impl;
 
 import com.argprogram.portfolio.dto.PortfolioDto;
+import com.argprogram.portfolio.mapper.EducationMapper;
+import com.argprogram.portfolio.mapper.ExperienceMapper;
+import com.argprogram.portfolio.mapper.InterestMapper;
 import com.argprogram.portfolio.mapper.PortfolioMapper;
+import com.argprogram.portfolio.mapper.ProjectMapper;
+import com.argprogram.portfolio.mapper.SkillMapper;
 import com.argprogram.portfolio.repository.PortfolioRepository;
+import com.argprogram.portfolio.service.EducationService;
+import com.argprogram.portfolio.service.ExperienceService;
+import com.argprogram.portfolio.service.InterestService;
 import com.argprogram.portfolio.service.PortfolioService;
+import com.argprogram.portfolio.service.ProjectService;
+import com.argprogram.portfolio.service.SkillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +23,23 @@ public class PortfolioServiceImpl implements PortfolioService {
 
     private final PortfolioRepository portfolioRepository;
     private final PortfolioMapper portfolioMapper;
+    private final ExperienceMapper experienceMapper;
+    private final EducationMapper educationMapper;
+    private final SkillMapper skillMapper;
+    private final ProjectMapper projectMapper;
+    private final InterestMapper interestMapper;
+    private final ExperienceService experienceService;
+    private final EducationService educationService;
+    private final SkillService skillService;
+    private final ProjectService projectService;
+    private final InterestService interestService;
 
     @Override
     public PortfolioDto getById(Long id) {
         PortfolioDto dto = this.portfolioRepository.findById(id)
                 .map(entity -> this.portfolioMapper.toPortfolioDto(entity))
                 .orElseThrow();
+        
         return dto;
     }
 
