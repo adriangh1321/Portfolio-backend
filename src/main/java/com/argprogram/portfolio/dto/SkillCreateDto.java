77e45d267c10d14ad126e248911088db29e17d06
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-23 20:40:19 -0300

    created SkillCreateDto

--- src/main/java/com/argprogram/portfolio/dto/SkillCreateDto.java
+++ src/main/java/com/argprogram/portfolio/dto/SkillCreateDto.java
@@ -1,7 +1,7 @@
+
 package com.argprogram.portfolio.dto;
 
 import com.argprogram.portfolio.enumeration.SkillType;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -11,31 +11,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-
-public class SkillDto {
-
-    private Long id;
-    
+@AllArgsConstructor
+@NoArgsConstructor
+public class SkillCreateDto {
+        
     @NotNull
     private SkillType type;
     
     @NotBlank
     private String name;
-
+    
     @NotNull
     @Max(value = 100, message = "The percent cannot be greater than 100")
     @Min(value = 0, message = "The percent cannot be less than 100")
     private Integer percent;
-
-    public SkillDto(
-            @JsonProperty(value = "id", required = false) Long id,
-            @JsonProperty(value = "type", required = true) SkillType type,
-            @JsonProperty(value = "name", required = true) String name,
-            @JsonProperty(value = "percent", required = true) Integer percent) {
-        this.id = id;
-        this.type = type;
-        this.name = name;
-        this.percent = percent;
-    }
-
+    
+    @NotNull
+    private Long idPortfolio;
 }
