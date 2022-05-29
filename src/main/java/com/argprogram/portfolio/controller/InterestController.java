3482e629d1c8e6ee6df944dd8f87726c721183c7
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-29 19:46:00 -0300

    Feat 16: Created InterestController, added save, update ,delete ,getAll and getAllByPortfolioId endpoints (#16)

---
+++ src/main/java/com/argprogram/portfolio/controller/InterestController.java
@@ -0,0 +1,60 @@
+
+package com.argprogram.portfolio.controller;
+
+import com.argprogram.portfolio.dto.ExperienceCreateDto;
+import com.argprogram.portfolio.dto.ExperienceDto;
+import com.argprogram.portfolio.dto.InterestCreateDto;
+import com.argprogram.portfolio.dto.InterestDto;
+import com.argprogram.portfolio.service.InterestService;
+import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/interests")
+@RequiredArgsConstructor
+public class InterestController {
+    
+    private final InterestService interestService;
+    
+    
+    @GetMapping(params = "portfolioId")
+    public ResponseEntity<List<InterestDto>> getAllByPortfolioId(@RequestParam("portfolioId") Long portfolioId) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.interestService.getAllByPortfolioId(portfolioId));
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<InterestDto>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(this.interestService.getAll());
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id,@RequestBody @Valid  InterestDto dto){
+        this.interestService.update(id, dto);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody @Valid InterestCreateDto dto){
+        this.interestService.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        this.interestService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    
+}
--- src/main/java/com/argprogram/portfolio/service/InterestService.java
+++ src/main/java/com/argprogram/portfolio/service/InterestService.java
@@ -1,13 +1,20 @@
-
 package com.argprogram.portfolio.service;
 
+import com.argprogram.portfolio.dto.InterestCreateDto;
 import com.argprogram.portfolio.dto.InterestDto;
 import java.util.List;
 
-
 public interface InterestService {
-    
+
     InterestDto getById(Long id);
-    
+
     List<InterestDto> getAllByPortfolioId(Long id);
+
+    List<InterestDto> getAll();
+
+    void update(Long id, InterestDto dto);
+
+    void save(InterestCreateDto dto);
+
+    void delete(Long id);
 }
