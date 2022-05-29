7ba395b257d4ef8ef08fcd98d28752cde536d1c6
Gustavo Hernández <adriangh1321@gmail.com>
2022-05-29 20:00:32 -0300

    created User and Role models

---
+++ src/main/java/com/argprogram/portfolio/model/Role.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.argprogram.portfolio.model;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Entity
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate updatedAt;
+
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }  
+}
