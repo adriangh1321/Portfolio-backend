f7b49e5cbfbb09a4fce36b350ec78733f4539e0e
Gustavo Hernández <adriangh1321@gmail.com>
2022-06-30 00:03:32 -0300

    added handleBadLoginException in GlobalControllerExceptionHandler

--- src/main/java/com/argprogram/portfolio/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/argprogram/portfolio/exception/GlobalControllerExceptionHandler.java
@@ -1,10 +1,23 @@
-
 package com.argprogram.portfolio.exception;
 
+import com.argprogram.portfolio.enumeration.ApplicationErrorCode;
+import com.argprogram.portfolio.enumeration.Location;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalControllerExceptionHandler {
-    
+public class GlobalControllerExceptionHandler extends AbstractExceptionHandler {
+
+    @ExceptionHandler(value = {BadLoginException.class})
+    protected ResponseEntity<ErrorDetails> handleBadLoginException(RuntimeException exc) {
+        ErrorDetails error = ErrorDetails.builder()
+                .code(ApplicationErrorCode.BAD_CREDENTIALS)
+                .description(exc.getMessage())
+                .location(Location.BODY)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
 }
